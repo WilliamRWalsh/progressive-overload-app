e29f92f1d5b89fad07bea6783fd1bb906e1f5b6a
WilliamRWalsh <williamrobertsonwalsh@gmail.com>
2023-04-02 20:31:30 -0400

    building out past sets

--- lib/exercise_page.dart
+++ lib/exercise_page.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ExercisePage extends StatelessWidget {
+class ExercisePage extends ConsumerWidget {
   const ExercisePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Bench Press'),
@@ -20,35 +21,138 @@ class ExercisePage extends StatelessWidget {
                 '30 secs',
                 style: Theme.of(context).textTheme.displayLarge,
               ),
-              IntrinsicHeight(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    SizedBox(
-                      width: 65,
-                      child: TextFormField(
-                        textAlign: TextAlign.center,
-                        keyboardType: TextInputType.number,
-                        initialValue: '100',
-                        inputFormatters: <TextInputFormatter>[
-                          FilteringTextInputFormatter.digitsOnly,
-                          LengthLimitingTextInputFormatter(2)
-                        ],
-                        onSaved: (String? value) {},
-                        validator: (String? value) => null,
-                      ),
+              Column(
+                children: [
+                  IntrinsicHeight(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SizedBox(
+                          width: 65,
+                          child: TextFormField(
+                            readOnly: true,
+                            textAlign: TextAlign.center,
+                            keyboardType: TextInputType.number,
+                            initialValue: '100',
+                            inputFormatters: <TextInputFormatter>[
+                              FilteringTextInputFormatter.digitsOnly,
+                              LengthLimitingTextInputFormatter(2)
+                            ],
+                            onSaved: (String? value) {},
+                            validator: (String? value) => null,
+                          ),
+                        ),
+                        const VerticalDivider(
+                          width: 20,
+                          thickness: 2,
+                          color: Colors.purple,
+                        ),
+                        const RepField(),
+                        const RepField(),
+                        const RepField(),
+                        const RepField(),
+                      ],
                     ),
-                    const VerticalDivider(
-                      width: 20,
-                      thickness: 2,
+                  ),
+                  IntrinsicHeight(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SizedBox(
+                          width: 65,
+                          child: TextFormField(
+                            readOnly: true,
+                            textAlign: TextAlign.center,
+                            keyboardType: TextInputType.number,
+                            initialValue: '100',
+                            inputFormatters: <TextInputFormatter>[
+                              FilteringTextInputFormatter.digitsOnly,
+                              LengthLimitingTextInputFormatter(2)
+                            ],
+                            onSaved: (String? value) {},
+                            validator: (String? value) => null,
+                          ),
+                        ),
+                        const VerticalDivider(
+                          width: 20,
+                          thickness: 2,
+                          color: Colors.purple,
+                        ),
+                        const RepField(),
+                        const RepField(),
+                        const RepField(),
+                        const RepField(),
+                      ],
+                    ),
+                  ),
+                  IntrinsicHeight(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SizedBox(
+                          width: 65,
+                          child: TextFormField(
+                            readOnly: true,
+                            textAlign: TextAlign.center,
+                            keyboardType: TextInputType.number,
+                            initialValue: '100',
+                            inputFormatters: <TextInputFormatter>[
+                              FilteringTextInputFormatter.digitsOnly,
+                              LengthLimitingTextInputFormatter(2)
+                            ],
+                            onSaved: (String? value) {},
+                            validator: (String? value) => null,
+                          ),
+                        ),
+                        const VerticalDivider(
+                          width: 20,
+                          thickness: 2,
+                          color: Colors.purple,
+                        ),
+                        const RepField(),
+                        const RepField(),
+                        const RepField(),
+                        const RepField(),
+                      ],
+                    ),
+                  ),
+                  const IntrinsicWidth(
+                    child: Divider(
                       color: Colors.purple,
+                      thickness: 2,
                     ),
-                    const RepField(),
-                    const RepField(),
-                    const RepField(),
-                    const AddRep(),
-                  ],
-                ),
+                  ),
+                  IntrinsicHeight(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SizedBox(
+                          width: 65,
+                          child: TextFormField(
+                            textAlign: TextAlign.center,
+                            keyboardType: TextInputType.number,
+                            initialValue: '100',
+                            inputFormatters: <TextInputFormatter>[
+                              FilteringTextInputFormatter.digitsOnly,
+                              LengthLimitingTextInputFormatter(2)
+                            ],
+                            onSaved: (String? value) {},
+                            validator: (String? value) => null,
+                          ),
+                        ),
+                        const VerticalDivider(
+                          width: 20,
+                          thickness: 2,
+                          color: Colors.purple,
+                        ),
+                        const RepField(),
+                        const RepField(),
+                        const RepField(),
+                        const RepField(),
+                      ],
+                    ),
+                  ),
+                ],
               ),
               SizedBox(
                 width: double.infinity,
@@ -70,13 +174,15 @@ class ExercisePage extends StatelessWidget {
 }
 
 class RepField extends StatelessWidget {
-  const RepField({Key? key}) : super(key: key);
+  const RepField({Key? key, this.readOnly = false}) : super(key: key);
+  final bool readOnly;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: 65,
       child: TextFormField(
+        readOnly: readOnly,
         textAlign: TextAlign.center,
         keyboardType: TextInputType.number,
         inputFormatters: <TextInputFormatter>[
@@ -87,18 +193,6 @@ class RepField extends StatelessWidget {
         onSaved: (String? value) {},
         validator: (String? value) => null,
       ),
-    );
-  }
-}
-
-class AddRep extends StatelessWidget {
-  const AddRep({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {},
-      child: const Center(child: const Icon(Icons.add)),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:progressive_overload_app/exercise_page.dart';
 
 void main() {
@@ -10,60 +11,62 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Progression Tracker',
-      theme: ThemeData(
-          scaffoldBackgroundColor: Colors.black87,
-          colorScheme: const ColorScheme(
-            brightness: Brightness.dark,
-            primary: Colors.purple,
-            onPrimary: Colors.black,
-            secondary: Colors.yellow,
-            onSecondary: Colors.yellow,
-            error: Colors.red,
-            onError: Colors.red,
-            background: Colors.black,
-            onBackground: Colors.black,
-            surface: Colors.black,
-            onSurface: Colors.grey,
-          ),
-          primarySwatch: Colors.purple,
-          textTheme: const TextTheme(
-            bodyLarge: TextStyle(color: Colors.white, fontSize: 20),
-            bodyMedium: TextStyle(color: Colors.white, fontSize: 16),
-            bodySmall: TextStyle(color: Colors.white, fontSize: 16),
-            displayLarge: TextStyle(color: Colors.white, fontSize: 46),
-            displayMedium: TextStyle(color: Colors.white, fontSize: 42),
-            displaySmall: TextStyle(color: Colors.white, fontSize: 36),
-          ),
-          inputDecorationTheme: const InputDecorationTheme(
-            focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                borderSide: BorderSide(color: Colors.purple, width: 2.5),
-                gapPadding: 30),
-            enabledBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                borderSide: BorderSide(
-                  color: Colors.white,
-                  width: 2.5,
-                ),
-                gapPadding: 30),
-            border: OutlineInputBorder(
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                borderSide: BorderSide(width: 10, color: Colors.white)),
-            iconColor: Colors.white,
-            labelStyle: TextStyle(
-              color: Colors.white,
-              decorationColor: Colors.white,
+    return ProviderScope(
+      child: MaterialApp(
+        title: 'Progression Tracker',
+        theme: ThemeData(
+            scaffoldBackgroundColor: Colors.black87,
+            colorScheme: const ColorScheme(
+              brightness: Brightness.dark,
+              primary: Colors.purple,
+              onPrimary: Colors.black,
+              secondary: Colors.yellow,
+              onSecondary: Colors.yellow,
+              error: Colors.red,
+              onError: Colors.red,
+              background: Colors.black,
+              onBackground: Colors.black,
+              surface: Colors.black,
+              onSurface: Colors.grey,
             ),
-            hintStyle: TextStyle(
-              color: Colors.white,
-              decorationColor: Colors.white,
+            primarySwatch: Colors.purple,
+            textTheme: const TextTheme(
+              bodyLarge: TextStyle(color: Colors.white, fontSize: 20),
+              bodyMedium: TextStyle(color: Colors.white, fontSize: 16),
+              bodySmall: TextStyle(color: Colors.white, fontSize: 16),
+              displayLarge: TextStyle(color: Colors.white, fontSize: 46),
+              displayMedium: TextStyle(color: Colors.white, fontSize: 42),
+              displaySmall: TextStyle(color: Colors.white, fontSize: 36),
             ),
-          )
-          // elevatedButtonTheme: ElevatedButtonThemeData(style: ButtonStyle(foregroundColor: ))
-          ),
-      home: const HomePage(),
+            inputDecorationTheme: const InputDecorationTheme(
+              focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  borderSide: BorderSide(color: Colors.purple, width: 2.5),
+                  gapPadding: 30),
+              enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  borderSide: BorderSide(
+                    color: Colors.white,
+                    width: 2.5,
+                  ),
+                  gapPadding: 30),
+              border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  borderSide: BorderSide(width: 10, color: Colors.white)),
+              iconColor: Colors.white,
+              labelStyle: TextStyle(
+                color: Colors.white,
+                decorationColor: Colors.white,
+              ),
+              hintStyle: TextStyle(
+                color: Colors.white,
+                decorationColor: Colors.white,
+              ),
+            )
+            // elevatedButtonTheme: ElevatedButtonThemeData(style: ButtonStyle(foregroundColor: ))
+            ),
+        home: const HomePage(),
+      ),
     );
   }
 }
